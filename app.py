47d7563e313cd32aead2bd312352500ddf5a67a8
Alok-Tungal <aloktungal39@gmail.com>
2025-08-20 11:47:27 +0530

    app.py

--- app.py
+++ app.py
@@ -1105,7 +1105,8 @@ small.mono { font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
 # HELPERS
 # ──────────────────────────────
 
-APP_URL = st.secrets.get("app_url", "").strip() or app_url = "https://pollutionappcreatedbyalok.streamlit.app/"
+APP_URL = st.secrets.get("app_url", "").strip() or "https://pollutionappcreatedbyalok.streamlit.app/"
+
 
 COLUMNS = ["PM2.5", "PM10", "NO2", "SO2", "CO", "Ozone"]
 
